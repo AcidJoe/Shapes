678752a4cc048fca518defe4819fd5b88a62f626
Evgeny Novikov <acidjoe1986@gmail.com>
2016-10-05 13:18:33 +0500

    Создал меню для тестов

---
+++ Assets/Scripts/Global/Profile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Profile
+{
+    public int id;
+    public string _name;
+    public string photo;
+
+    public int money;
+
+    public int lvl;
+    public int exp;
+    public int exp_to_next;
+
+    public Profile(string n)
+    {
+        _name = n;
+
+        lvl = 1;
+        exp = 0;
+
+        money = 1000;
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject loginPanel;
+    public GameObject mainMenu;
+
+    public Text PlayerName;
+    public Text Money;
+
+    public string playerName;
+
+	void Start ()
+    {
+        if (!Game.isReady)
+        {
+            loginPanel.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+
+	}
+	
+	void Update ()
+    {
+        if (Game.isReady)
+        {
+            PlayerName.text = Game.player._name;
+            Money.text = Game.player.money.ToString();
+        }
+	}
+
+    public void SetName(string s)
+    {
+        playerName = s;
+    }
+
+    public void CreateProfile()
+    {
+        Game.player = new Profile(playerName);
+        loginPanel.SetActive(false);
+        Game.isReady = true;
+        mainMenu.SetActive(true);
+    }
+}
